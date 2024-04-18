a404b19493a9d091a0b1ce290c479ff7c8da6bf8
Anushree-hs <143162193+Anushree-hs@users.noreply.github.com>
2024-04-18 19:10:55 +0530

    Update caeser_cipher.py
    Updated caeser function contains both encryption and decryption

--- caeser_cipher.py
+++ caeser_cipher.py
@@ -2,6 +2,25 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
+# Both encryption and decryption in one function
+# def caesar(start_text, shift_amount, cipher_direction):
+#   end_text = ""
+#   if cipher_direction == "decode":
+#     shift_amount *= -1
+#   for char in start_text:
+#     if char in alphabet:
+#       position = alphabet.index(char)
+#       new_position = position + shift_amount
+#       end_text += alphabet[new_position]
+#     else:
+#       end_text += char
+#   print(f"The {cipher_direction}d text is {end_text}")
+
+# caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+# Both encryption and decryption in different function
+# Encryption function
 def encrypt(plain_text, shift_amount):
   cipher_text = ""
   for letter in plain_text:
@@ -10,8 +29,8 @@ def encrypt(plain_text, shift_amount):
     new_letter = alphabet[new_position]
     cipher_text += new_letter
   print(f"The encoded text is {cipher_text}")
-  
 
+# Decryption function
 def decrypt(cipher_text, shift_amount):
   plain_text = ""
   for letter in cipher_text:
